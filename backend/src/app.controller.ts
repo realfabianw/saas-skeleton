b96942221c88f946537d88730b83e8ec7b3d5c80
Fabian Werling <Werling.Fabian@gmail.com>
2024-06-18 22:47:04 +0200

    code cleanup

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -6,7 +6,6 @@ export class AppController {
   private readonly logger = new Logger(AppController.name);
   @Get('ping')
   ping(@Req() request: Request, @Res() response: Response) {
-    this.logger.log('Request: ' + request['user']);
     response.status(200).send();
   }
 }
