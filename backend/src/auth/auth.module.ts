9d2402e7f6438436594a0c230166661150f44437
Fabian Werling <Werling.Fabian@gmail.com>
2024-06-19 00:36:05 +0200

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,18 +3,19 @@ import { ClerkJwtStrategy } from './clerk-jwt.strategy';
 import { ClerkJwtAuthGuard } from './clerk-jwt-auth.guard';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersService } from '../users/users.service';
 
 /**
  * The AuthModule is responsible for providing the JWT strategy and guard for authenticating Clerk JWTs.
  * All endpoints are protected by default. Protection can be disabled via a custom decorator.
  */
 @Module({
-  imports: [],
   controllers: [AuthController],
   providers: [
     ClerkJwtStrategy,
     { provide: 'APP_GUARD', useClass: ClerkJwtAuthGuard },
     AuthService,
+    UsersService,
   ],
 })
 export class AuthModule {}
